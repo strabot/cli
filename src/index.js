4bc135486d1640f1202fad917ffb8593db30bee1
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-10 15:47:41 -0300

    Initial command create

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Strabot CLI')
+#!/usr/bin/env node
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import { create } from './handlers/create.js'
+
+yargs(hideBin(process.argv))
+  .command('create [component]', 'Creates a strabot component', (yargs) => {
+    return yargs
+      .positional('component', {
+        choices: ['bot', 'manager'],
+        describe: 'Strabot component like manager or bot',
+        type: 'string'
+      })
+  }, create)
+  .option('name', {
+    alias: 'n',
+    description: 'The project/folder name',
+    type: 'string'
+  })
+  .option('platform', {
+    alias: 'p',
+    choices: ['discord', 'slack', 'telegram'],
+    type: 'string'
+  })
+  .parse()

466c410a91d99a7539f4d8da57189acb09b84aa3
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-24 16:16:35 -0300

    Lint fix

--- src/handlers/populate.js
+++ src/handlers/populate.js
@@ -8,7 +8,7 @@ export async function populate () {
 
   try {
     dotenv.config({ path: join(process.cwd(), '.env') })
-  
+
     const database = knex({
       client: 'sqlite3',
       connection: {
@@ -16,7 +16,7 @@ export async function populate () {
       },
       useNullAsDefault: true
     })
-  
+
     const [messageId] = await database('messages').insert({
       label: 'Welcome',
       text: "Hey! It's so nice to meet you  and I'm glad you're here!\nPresent yourself so I can know more about you :D ",
@@ -24,17 +24,17 @@ export async function populate () {
       updated_at: Date.now(),
       published_at: Date.now()
     })
-  
+
     const [greetingId] = await database('greetings').insert({
       created_at: Date.now(),
       updated_at: Date.now()
     })
-  
+
     await database('greetings_messages_links').insert({
       greeting_id: greetingId,
       message_id: messageId
     })
-  
+
     await database.destroy()
 
     loading.succeed()
